8ad23b95411de477be19e73ad5b7fc8e4c29f774
Chay Paterson <chaypaterson@gmail.com>
2023-04-28 13:54:46 +0100

    fixed bug! was not calculating probability in the right place

--- ts-loss-characteristic-errors.cpp
+++ ts-loss-characteristic-errors.cpp
@@ -7,9 +7,18 @@
 #include "graph-model-spec.hpp"
 #include "flying-conjugates.hpp"
 
-// A conjugate characteristics simulation of tumour suppressor loss
+/* A conjugate characteristics simulation of tumour suppressor loss
+ * This program estimates the global error with Richardson extrapolation.
+ */
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "please provide a time step: \n";
+        std::cout << "  ./this_program time_step";
+        std::cout << std::endl;
+        return 1;
+    }
+
     // System coefficients:
     real_t rloh = 0.5e-2;
     real_t mu = 0.5e-3;
@@ -26,27 +35,29 @@ int main(int argc, char* argv[]) {
     model.m_initial_pops = {1e2, 0, 0, 0, 0};
 
     // final vertices 3 and 4: 4 = mutants with LOH
-    // TODO new conditions: what about the probability to end up on 3, the
-    // probability to end up on 3 or 4, and the probability to end up on 4 GIVEN
-    // THAT 3 or 4 have been visited?
     std::vector<real_t> qvalues = {1, 1, 1, 1, 0};
+    std::vector<real_t> qvalues2 = qvalues;
 
     real_t time = 0.0;
     const real_t tmax = 100.0;
     real_t dt = atof(argv[1]); // integration step
+    real_t half = 0.5 * dt;
     real_t t_write_step = 1.0; // write out step
 
     // print key:
-    std::cout << "age, p1, p2," << std::endl;
-    std::cout.precision(17);
+    std::cout << "age, err," << std::endl;
+    std::cout.precision(20);
     // print first line:
-    std::cout << std::fixed << time << ", " << 1.0 << ", ";
+    std::cout << std::fixed << time << ", ";
     std::cout << std::fixed << 0.0 << ", " << std::endl;
     real_t t_write = time + t_write_step;
 
     while (time < tmax) {
         while (time < t_write - dt) {
             heun_q_step(qvalues, time, dt, model);
+            // Advance qvalues 2 by half the relevant time step, twice
+            heun_q_step(qvalues2, time, half, model);
+            heun_q_step(qvalues2, time, half, model);
             // Increment time:
             time += dt;
         }
@@ -54,11 +65,19 @@ int main(int argc, char* argv[]) {
         {
             real_t delta = t_write - time;
             heun_q_step(qvalues, time, delta, model);
+            real_t halfdelta = 0.5 * delta;
+            heun_q_step(qvalues2, time, halfdelta, model);
+            heun_q_step(qvalues2, time, halfdelta, model);
             time = t_write;
-            // Write out probabilities:
+            // compute the P(t) values:
             real_t prob = generating_function(qvalues, model.m_initial_pops);
-            std::cout << std::fixed << time << ", " << prob << ", ";
-            std::cout << std::fixed << 1.0 - prob << ", " << std::endl;
+            real_t prob2 = generating_function(qvalues2, model.m_initial_pops);
+            // compute the corresponding error:
+            double err = (prob - prob2);
+            err /= (2 << 2 - 1); // richardson extrapolation
+            err *= (1 - 2 * (err < 0)); // abs value
+            // Write out errors:
+            std::cout << std::fixed << time << ", " << err << std::endl;
             t_write += t_write_step;
         }
     }
---
+++ tstester.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# "1e-9" "1e-8"  TODO
+dts=("1e-7" "1e-6" "1e-5" "1e-4" "1e-3" "1e-2" "3e-7" "3e-6" "3e-5" "3e-4" "3e-3" "3e-2")
+
+for dt in ${dts[*]}
+do 
+    echo $dt > errorpropagation/results_$dt.csv
+    ./bin/tsfferrs $dt >> errorpropagation/results_$dt.csv
+done
