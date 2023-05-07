3763452d4cd9c4553b767bfbceed5b514d99cbd2
Chay Paterson <chaypaterson@gmail.com>
2023-05-07 17:28:03 +0100

    implemented histogram-based version of likelihood maximisation, annealing routine now uses closures

--- parameter_inference/likelihood-optimisation.cpp
+++ parameter_inference/likelihood-optimisation.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <functional>
 
 #include "../graph-model-spec.hpp"
 #include "../flying-conjugates.hpp"
@@ -49,8 +50,8 @@ void map_onto_data(Model& params, const epidata_t& this_data,
         // advance one dt step into the future:
         heun_q_step(qvals, time, dt, params);
         real_t prob2 = generating_function(qvals, params.m_initial_pops);
-        // capture derivative, this is the hazard:
-        real_t dprob = prob - prob2;
+        // capture derivative of probabilities, this will give the hazard:
+        real_t dprob = -(prob2 - prob);
         dprob /= dt;
 
         // apply the function to the arguments:
@@ -86,30 +87,104 @@ real_t logsurvival(Model& params, int node) {
     return -log(psurv);
 }
 
-real_t endcorrection(Model& params, real_t max_age, size_t population) {
-    // return the end correction to the log likelihood, that is independent of
+real_t endcorrection(Model& params, real_t max_age, size_t node, size_t population) {
+    // return the end correction to the -log likelihood, that is independent of
     // the data:
     real_t Lambda = 0;
     real_t dt = 0.01;
     
-    size_t endnodes[] = {3, 4}; // TODO generalise
-    for (auto& node : endnodes) {
-        std::vector<real_t> qvals(params.m_stages, 1);
-        qvals[node] = 0;
-        // integrate to get likelihood:
-        real_t time = 0.0;
-        while (time < max_age) {
-            heun_q_step(qvals, time, dt, params);
-            time += dt;
-        }
-
-        real_t prob = generating_function(qvals, params.m_initial_pops);
-        Lambda += -log(prob);
+    std::vector<real_t> qvals(params.m_stages, 1);
+    qvals[node] = 0;
+    // integrate to get likelihood:
+    real_t time = 0.0;
+    while (time < max_age) {
+        heun_q_step(qvals, time, dt, params);
+        time += dt;
     }
+
+    real_t prob = generating_function(qvals, params.m_initial_pops);
+    Lambda += -log(prob);
+
     return population * Lambda;
 }
 
+real_t loglikelihood_hist_node(Model& params, size_t node, real_t binwidth,
+                            size_t ref_population,
+                            const std::vector<size_t>& freqs) {
+    // Histogram version of the -log likelihood
+    // Recieves a histogram of cancers with a known type (node)
+    // Returns a -log Poisson likelihood
+    real_t energy = 0;
+
+    real_t time = 0;
+    std::vector<real_t> qvals(params.m_stages, 1);
+    qvals[node] = 0;
+    real_t end_time = binwidth;
+    real_t dt = 0.01;
+
+    for (const size_t& curr_bin : freqs) {
+        // integrate the hazard over the bin:
+        real_t prob = generating_function(qvals, params.m_initial_pops);
+        while (time < end_time) {
+            heun_q_step(qvals, time, dt, params);
+            time += dt;
+        }
+        real_t prob2 = generating_function(qvals, params.m_initial_pops);
+        // Compute the expected number of cases in this bin:
+        real_t hazard = -(prob2 - prob)/prob;
+        real_t Lambda = ref_population * prob * hazard;
+        // -log Poisson likelihood:
+        energy += -log(Lambda) * curr_bin;
+        energy += +Lambda;
+        // weight for survival/chance of detection of cancer:
+        energy += logsurvival(params, node) * curr_bin;
+        // update the end time for the next pass:
+        end_time += binwidth;
+    }
+
+    return energy;
+}
+
+real_t loglikelihood_hist_both(Model& params, real_t binwidth,
+                               size_t ref_population,
+                               std::map<size_t, std::vector<size_t>> histos) {
+    // Histogram version of the -log likelihood
+    // Applied to a set of histograms of cancer incidence with given types
+    // The data structure maps the type to a histogram that gives age incidence
+    real_t energy = 0;
+
+    for (const auto& type : histos) {
+        energy += loglikelihood_hist_node(params, type.first, binwidth,
+                                          ref_population, type.second);
+    }
+
+    return energy;
+}
+
+std::vector<size_t> convert_to_histogram(const epidata_t& all_times, 
+                                         real_t binwidth,
+                                         size_t node) {
+    real_t max_age = 0;
+    for (auto& datum : all_times) {
+        if (datum.first > max_age) max_age = datum.first;
+    }
+
+    size_t num_bins = max_age / binwidth + 1;
+
+    std::vector<size_t> histogram(num_bins, 0);
+
+    for (auto& entry : all_times) {
+        if (entry.second == node) {
+            size_t bin = entry.first / binwidth;
+            histogram[bin]++;
+        }
+    }
+
+    return histogram;
+}
+
 real_t loglikelihood(Model& params, const epidata_t& all_times) {
+    // TODO need a version that acts on histograms instead of a list of ages
     real_t energy = 0, max_age = 0;
 
     map_onto_data(params, all_times, logdprob, &energy);
@@ -119,11 +194,17 @@ real_t loglikelihood(Model& params, const epidata_t& all_times) {
         max_age = max_age * (max_age > datum.second) + 
                   datum.second * (max_age < datum.second);
     }
+    // apply the end correction for the finite reference population:
     size_t population = all_times.size();
-    energy + endcorrection(params, max_age, population);
+    size_t endnodes[] = {3, 4}; // TODO generalise
+    for (auto& node : endnodes) {
+        energy += endcorrection(params, max_age, node, population);
+    }
 
     return energy;
 }
+
+// Gillespie algorithm functions:
 
 std::vector<std::pair<double,int>> generate_dataset(int seed, int runs) {
     // System coefficients:
@@ -214,7 +295,10 @@ Model get_neighbour(Model& model, double w) {
 }
  
 real_t estimate_error(Model& old_model, Model& new_model) {
-    // compute square % difference in model parameters
+    // TODO this function is ugly
+    // There should be a nice internal method in the Model class for iterating
+    // over parameters TODO implement this
+    // compute the square % difference in model parameters
     real_t error = 0;
     if (old_model.m_stages != new_model.m_stages) return error;
     for (size_t v_in = 0; v_in < old_model.m_stages; ++v_in) {
@@ -223,12 +307,15 @@ real_t estimate_error(Model& old_model, Model& new_model) {
                           log(new_model.m_migr[v_in][v_out]);
             if (std::isfinite(diff)) error += diff * diff;
         }
+
         real_t diff = log(old_model.m_birth[v_in]) -
                       log(new_model.m_birth[v_in]);
         if (std::isfinite(diff)) error += diff * diff;
+
         diff = log(old_model.m_death[v_in]) -
                log(new_model.m_death[v_in]);
         if (std::isfinite(diff)) error += diff * diff;
+
         diff = log(old_model.m_initial_pops[v_in]) -
                log(new_model.m_initial_pops[v_in]);
         if (std::isfinite(diff)) error += diff * diff;
@@ -237,17 +324,30 @@ real_t estimate_error(Model& old_model, Model& new_model) {
     return sqrt(error);
 }
 
-Model annealing_min(double (*objective)(Model& model, const epidata_t& dataset),
-                    Model initial_guess, const epidata_t& dataset) {
+/* MAYBE TODO define a class that we can use to "capture" or hide the extraneous
+ * arguments like the underlying data and parameters of the histogram. We should
+ * be able to pass one object into annealing_min and call it like a function
+ * with "objective(model);". This should make the signature of annealing_min
+ *      Model annealing_min(ObjectiveFunc objective, Model initial_guess);
+ *
+ * CURRENTLY I am passing a closure to this object -- this allows the other
+ * parameters of the log-likelihood objective function to be "bound" in the
+ * context.
+ */
+Model annealing_min(std::function<real_t(Model& model)> objective, 
+                    Model initial_guess) {
+    // Function reads a set of datum points and returns a set of model
+    // parameters.
     // Constants:
-    const double Tmin = 1e-8; // Minimal temperature
+    const double Tmin = 1e-16; // Minimal temperature
     const double delta = 0.98; // The rate of temperature drop
     const double min_width = 1e-2f; // i.e. 1% of the log-cauchy variate
+    const double smoothing_factor = 0.03; // for smoothing of error estimates
 
     // Initialise variables:
     Model model = initial_guess;
     Model best_guess = model;
-    double best_y = objective(best_guess, dataset);
+    double best_y = objective(best_guess);
     double old_best_y = best_y; // Used for adaptive width size
 
     // Width of neighbourhood:
@@ -266,20 +366,25 @@ Model annealing_min(double (*objective)(Model& model, const epidata_t& dataset),
     unsigned int iter = 0;  // count iterations
     double err_est = 0;
 
-    while ((Temp > Tmin) || (err_est > min_width)) {
+    while ((Temp > Tmin)){// || (err_est > min_width)) {
         Model new_guess = get_neighbour(model, w);
        
-        double y_new = objective(new_guess, dataset);
+        double y_new = objective(new_guess);
+
         if (std::isnan(y_new)) continue;
+
         double delta_y = y_new - best_y;
 
         if ((delta_y < 0) || ((rand() / (RAND_MAX + 1.0)) < exp(-delta_y / Temp))) {
-            err_est = estimate_error(new_guess, best_guess);
+            err_est *= 1.0 - smoothing_factor;
+            err_est += smoothing_factor * estimate_error(new_guess, best_guess);
             model = new_guess;
             best_guess = model;
             best_y = y_new;
         }
+
         Temp *= delta;
+
         /* If best_y has improved by more than the current temperature, or the
          * most recent change was too small, shrink the width: */
         if (((old_best_y - best_y) > Temp) || (err_est < w / 8)) {
@@ -313,25 +418,62 @@ int main(int argc, char* argv[]) {
     std::cout << "Done. Saving..." << std::endl;
 
     // save the fake data:
-    std::ofstream fakedata;
-    fakedata.open("syntheticdata.csv");
-    for (auto& entry : all_times) {
-        fakedata << entry.first << "," << entry.second << "," << std::endl;
+    {
+        std::ofstream fakedata;
+        fakedata.open("syntheticdata_raw.csv");
+        for (auto& entry : all_times) {
+            fakedata << entry.first << "," << entry.second << "," << std::endl;
+        }
+        fakedata.close();
     }
-    fakedata.close();
+
+    // Convert age data to histogram:
+    real_t binwidth = 1.0f; // years
+    std::map<size_t, std::vector<size_t>> incidence;
+    incidence[3] = convert_to_histogram(all_times, binwidth, 3);
+    incidence[4] = convert_to_histogram(all_times, binwidth, 4);
+    size_t reference_pop = all_times.size();
+
+    // Save the histogram:
+    {
+        std::ofstream fakedata;
+        fakedata.open("syntheticdata_hist.csv");
+        fakedata << "bin width = " << binwidth << "\n";
+        fakedata << "max age = " << binwidth * incidence[3].size() << "\n";
+        fakedata << "ref population = " << reference_pop << std::endl;
+
+        fakedata << "[";
+        for (auto& bin : incidence[3]) 
+            fakedata << bin << ", ";
+        fakedata << "]" << std::endl;
+        fakedata << "[";
+        for (auto& bin : incidence[4])
+            fakedata << bin << ", ";
+        fakedata << "]" << std::endl;
+
+        fakedata.close();
+    }
 
     // Guess some initial model parameters:
-    real_t rloh = 0.001;
-    real_t mu = 0.001;
+    real_t rloh = 0.1;
+    real_t mu = 0.1;
     real_t fitness1 = 0.1;
     real_t fitness2 = 0.1;
-    real_t initialpop = 100;
+    real_t initialpop = 10;
 
     Model guess = instantiate_model(rloh, mu, fitness1, fitness2, initialpop);
 
     // Try out simulated annealing:
     std::cout << "Starting annealing..." << std::endl;
-    Model best_guess = annealing_min(loglikelihood, guess, all_times);
+    std::function<real_t(Model&)> objective = [&](Model& model) {
+        //return loglikelihood(model, all_times); // non-histogram version
+        return loglikelihood_hist_both(model, binwidth, 
+                                       reference_pop, incidence);
+        // the histogram version
+    };
+
+    Model best_guess = annealing_min(objective, guess);
+    // Annealing now complete. Store parameters and print:
     mu   = best_guess.m_migr[0][1];
     rloh = best_guess.m_migr[0][2];
     fitness1 = best_guess.m_birth[1];
